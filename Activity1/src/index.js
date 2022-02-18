4ae8b916d18fce3fa196f9a665881a5f7f376328
junyalcuizar822 <fortunato.alcuizar@gmail.com>
2022-02-18 23:20:39 +0800

    first commit

---
+++ Activity1/src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello, Fortunato D. Alcuizar III!</h1>
+<div>
+Fortunato is currently a 3rd Year Student taking up to course of BSIT in La Salle University
+Ozamiz City. You can add me on my facebook account by clicking this
+  <a href="https://www.facebook.com/Junehhhh/" target="_blank" rel="noopener noreferrer">button</a>.
+  </div>
+`;
